fa64741dccdea4e2babbfd10c90ae62a6a4d7a0d
tcsyxx6350 <tcsyxx6350@4a1214cc-c0b1-f683-8923-9db744e678e7>
2011-01-01 11:01:25 +0000

    try

---
+++ trunk/index.php
@@ -0,0 +1,43 @@
+<!--<html>
+<head><title>Tour Information</title></head>
+<body>
+<h2>Mission Information</h2>
+<form action="formaction.php">
+  <table width="100%">
+    <tr><td>Ship Name:</td><td><input type="text" name="ship" /></td></tr>
+    <tr><td>Trip Date:</td><td><input type="text" name="tripdate" /></td></tr>
+    <tr><td colspan="2">Mission Goals:</td></tr>
+    <tr>
+      <td><input type="checkbox" name="exploration" value="yes" />
+               Exploration</td>
+      <td><input type="checkbox" name="contact" value="yes" />
+               Contact</td>
+    </tr>
+    <tr>
+      <td valign="top">Crew species:  </td>
+      <td>
+        <select name="crew" multiple="multiple">
+           <option value="xebrax">Xebrax</option>
+           <option value="snertal">Snertal</option>
+           <option value="gosny">Gosny</option>
+        </select>
+      </td>
+    </tr>
+    <tr><td colspan="2" align="center"><input type="submit" /></td></tr>
+  </table>
+</form>
+</body>
+</html>
+-->
+
+<html>
+  <head><title>register & login</title></head>
+  <body>
+    <form action="login.php" method="POST" name="hi">
+      <tr><td>username</td><td><input type="text" name="username"/></td></tr>
+      <tr><td>password</td><td><input type="password" name="passwd"/></td></tr>
+      <tr><td><input type="submit" /></td></tr>
+    </form>
+  </body>
+</html>
+
